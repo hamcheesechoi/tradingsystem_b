6bac7c6ed781c9ce35c4fbfdf425fc2fd0b91c96
jw <choi2372@gmail.com>
2026-06-01 10:44:10 +0900

    buy_nice_timing, sell_nice_timing 구현을 위한 unit test 4r

--- tradingsystem/app.cpp
+++ tradingsystem/app.cpp
@@ -1,9 +1,11 @@
-class auto_trading_system {
-	void buy_nice_timing() {
+#pragma once
+#include "driver.cpp"
 
+class auto_trading_system {
+public:
+	void buy_nice_timing() {
 	}
 
 	void sell_nice_timing() {
-
 	}
 };
