81dfc9737611f623a2899c87a950b67b42987ef8
wally79 <wally79@naver.com>
2026-06-01 13:27:23 +0900

    app test를 test fixture로 refactoring

--- tradingsystem/app_test.cpp
+++ tradingsystem/app_test.cpp
@@ -12,23 +12,32 @@ public:
     MOCK_METHOD(int, currentPrice, (std::string stockCode, int minute), (override));
 };
 
-TEST(App, TC1) {
+class AppTest : public Test {
+protected:
+    MockBrocker mock;
+    auto_trading_system app{ &mock };
+
+    std::string stockCode = "005930";
+    int budget = 1000000;
+    int holdCount = 50;
+
+    // 3회 가격 조회 결과를 설정한다 (200ms 주기)
+    void setPrices(int price1, int price2, int price3) {
+        EXPECT_CALL(mock, currentPrice(stockCode, 200))
+            .WillOnce(Return(price1))
+            .WillOnce(Return(price2))
+            .WillOnce(Return(price3));
+    }
+};
+
+TEST_F(AppTest, BuyOnUpTrend) {
     // buy nice timing
     // 200ms 주기로 3회 가격을 읽고, 가격이 올라가는 추세인지 파악
     // 가격이 올라가는 추세라면, 총 금액을 최대한 사용하여 최대 수량만큼 매수한다.
     // 마지막에 읽은 가격으로 매수한다.
 
-    MockBrocker mock;
-    auto_trading_system app(&mock);
-
-    std::string stockCode = "005930";
-    int budget = 1000000;
-
     // 3회 가격 조회: 5000 → 5100 → 5200 (상승 추세)
-    EXPECT_CALL(mock, currentPrice(stockCode, 200))
-        .WillOnce(Return(5000))
-        .WillOnce(Return(5100))
-        .WillOnce(Return(5200));
+    setPrices(5000, 5100, 5200);
 
     // 마지막 가격(5200)으로 최대 수량 매수: 1000000 / 5200 = 192
     EXPECT_CALL(mock, buy(stockCode, 5200, 192))
@@ -37,20 +46,11 @@ TEST(App, TC1) {
     app.buy_nice_timing(stockCode, budget);
 }
 
-TEST(App, TC2) {
+TEST_F(AppTest, DoNotBuyOnDownTrend) {
     // buy nice timing - 하락 추세일 때 매수하지 않음
 
-    MockBrocker mock;
-    auto_trading_system app(&mock);
-
-    std::string stockCode = "005930";
-    int budget = 1000000;
-
     // 3회 가격 조회: 5200 → 5100 → 5000 (하락 추세)
-    EXPECT_CALL(mock, currentPrice(stockCode, 200))
-        .WillOnce(Return(5200))
-        .WillOnce(Return(5100))
-        .WillOnce(Return(5000));
+    setPrices(5200, 5100, 5000);
 
     // 하락 추세이므로 매수하지 않음
     EXPECT_CALL(mock, buy(_, _, _))
@@ -59,22 +59,13 @@ TEST(App, TC2) {
     app.buy_nice_timing(stockCode, budget);
 }
 
-TEST(App, TC3) {
+TEST_F(AppTest, SellOnDownTrend) {
     // sell nice timing
     // 200ms 주기로 3회 가격을 읽고, 가격이 내려가는 추세인지 파악
     // 가격이 내려가는 추세라면, 보유 수량 전량을 마지막에 읽은 가격으로 매도한다.
 
-    MockBrocker mock;
-    auto_trading_system app(&mock);
-
-    std::string stockCode = "005930";
-    int holdCount = 50;
-
     // 3회 가격 조회: 5200 → 5100 → 5000 (하락 추세)
-    EXPECT_CALL(mock, currentPrice(stockCode, 200))
-        .WillOnce(Return(5200))
-        .WillOnce(Return(5100))
-        .WillOnce(Return(5000));
+    setPrices(5200, 5100, 5000);
 
     // 마지막 가격(5000)으로 전량 매도
     EXPECT_CALL(mock, sell(stockCode, 5000, holdCount))
@@ -83,20 +74,11 @@ TEST(App, TC3) {
     app.sell_nice_timing(stockCode, holdCount);
 }
 
-TEST(App, TC4) {
+TEST_F(AppTest, DoNotSellOnUpTrend) {
     // sell nice timing - 상승 추세일 때 매도하지 않음
 
-    MockBrocker mock;
-    auto_trading_system app(&mock);
-
-    std::string stockCode = "005930";
-    int holdCount = 50;
-
     // 3회 가격 조회: 5000 → 5100 → 5200 (상승 추세)
-    EXPECT_CALL(mock, currentPrice(stockCode, 200))
-        .WillOnce(Return(5000))
-        .WillOnce(Return(5100))
-        .WillOnce(Return(5200));
+    setPrices(5000, 5100, 5200);
 
     // 상승 추세이므로 매도하지 않음
     EXPECT_CALL(mock, sell(_, _, _))
