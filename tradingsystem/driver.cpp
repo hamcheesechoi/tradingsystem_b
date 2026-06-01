9b66719757f76b4cb6b239fc4165662da2deda23
badgoofyman <badgoofy@gmail.com>
2026-06-01 10:22:25 +0900

    driver: driver argument

--- tradingsystem/driver.cpp
+++ tradingsystem/driver.cpp
@@ -1,8 +1,10 @@
+#include <string>
+
 #define interface struct
 
 interface StockerBrocker{
-	virtual void buy() = 0;
-	virtual void sell() = 0;
-	virtual void currentPrice() = 0;
-	virtual void login() = 0;
+	virtual void buy(std::string stockCode, int price, int count) = 0;
+	virtual void sell(std::string stockCode, int price, int count) = 0;
+	virtual void currentPrice(std::string stockCode, int minute) = 0;
+	virtual void login(std::string ID, std::string pass) = 0;
 };
