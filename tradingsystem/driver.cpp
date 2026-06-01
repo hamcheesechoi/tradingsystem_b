24008de3d1ef2497b99d1243b483f934c4278f49
badgoofyman <badgoofy@gmail.com>
2026-06-01 11:08:26 +0900

    driver: currentPrice 리턴값 오류 수정

--- tradingsystem/driver.cpp
+++ tradingsystem/driver.cpp
@@ -5,6 +5,6 @@
 interface StockerBrocker{
 	virtual void buy(std::string stockCode, int price, int count) = 0;
 	virtual void sell(std::string stockCode, int price, int count) = 0;
-	virtual void currentPrice(std::string stockCode, int minute) = 0;
+	virtual int currentPrice(std::string stockCode, int minute) = 0;
 	virtual void login(std::string ID, std::string pass) = 0;
 };
